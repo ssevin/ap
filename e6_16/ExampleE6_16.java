0e9733c94c92c5818182318e7383edaf9247c1fa
Sevin Nasiri <sevinnasiri85@gmail.com>
2025-03-07 17:27:02 +0330

    Example E6_16.

---
+++ e6_16/ExampleE6_16.java
@@ -0,0 +1,14 @@
+package e6_16;
+
+
+public class ExampleE6_16 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i * j + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
